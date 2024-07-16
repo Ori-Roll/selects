fa573b9400cc0ac974b575a76eea3cca9d78d903
Ori Roll <oriroll@gmail.com>
2024-07-16 11:14:30 +0300

    new Input simple - basic impl - ts - css form css updats to select css

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,23 @@
+import style from './input.module.css';
+
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = (props: InputProps) => {
+  const { onChange, ...restInputProps } = props;
+
+  return (
+    <input
+      className={style.input}
+      type="text"
+      onChange={onChange}
+      {...restInputProps}
+    />
+  );
+};
+
+export default Input;

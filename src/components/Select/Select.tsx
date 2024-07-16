c98e69734d76446a83797db597da1020843a8048
Ori Roll <oriroll@gmail.com>
2024-07-16 10:05:57 +0300

    Better Select added better functionality for handleSelect - using multi - setting open correctly - using only array for selected - using placeholder logic - better handle input change Type checking now works with multi and single - no more type error for filter and onSelect Style for select on module

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,29 +1,121 @@
+import { useState } from 'react';
+import DropdownInput from '../DropdownInput/DropdownInput';
+import style from './select.module.css';
+import { filterOptions } from './utils';
+import { Checkbox } from '../Checkbox/Checkbox';
+
+// TODO: Maybe add a debounce to the select (and for the input if implementing BE filtering)
+
+const DEFAULT_PLACEHOLDER = 'Select an option';
+
+export type Option = { id: string; label: string };
+
 type MultiSelectProps = {
   multiple: true;
+  selected?: string[];
+  onSelect: (
+    selectedItems: string[],
+    e?: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => void;
+};
+
+type SingleSelectProps = {
+  multiple?: false;
+  selected?: string;
+  onSelect: (
+    selectedItems: string | null,
+    e?: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => void;
+};
+
+type SelectBaseProps = {
+  options: Option[];
+  placeholder?: string;
+} & (MultiSelectProps | SingleSelectProps);
+
 const TestMultiSelect = (props: SelectBaseProps) => {
   const {
     options,
+    onSelect,
+    multiple,
+    selected,
+    placeholder = DEFAULT_PLACEHOLDER,
+  } = props;
+
+  const initSelectedItems = () => {
+    if (!selected) return [];
+    return Array.isArray(selected) ? selected : [selected];
+  };
+
   const [open, setOpen] = useState(false);
   const [selectedItems, setSelectedItems] = useState(initSelectedItems);
   const [filteredOptions, setFilteredOptions] = useState<Option[]>(options);
+
   const handleSelect = (
     option: Option,
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
+    if (multiple) {
+      const newSelectedValues = selectedItems?.includes(option.id)
+        ? selectedItems.filter((optionId) => optionId !== option.id)
+        : [...(selectedItems || []), option.id];
+      setSelectedItems(newSelectedValues);
+      onSelect(newSelectedValues, e);
+      return;
+    }
+    if (!selectedItems?.includes(option.id)) {
+      setSelectedItems([option.id]);
+      onSelect(option.id, e);
+      setOpen(false);
+    } else {
+      setSelectedItems([]);
+      onSelect(null, e);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilteredOptions(filterOptions(e.target.value, options));
+  };
+
+  const findOptionLabel = (id: string) =>
+    options.find((option) => option.id === id)?.label;
+
+  const getPlaceholder = () => {
+    if (multiple && selectedItems?.length) {
+      return selectedItems.map((id) => findOptionLabel(id)).join(', ');
+    } else if (!multiple && selectedItems?.length) {
+      return findOptionLabel(selectedItems[0]);
+    }
+    return placeholder;
+  };
+
   return (
     <div>
       <DropdownInput
         open={open}
         setOpen={setOpen}
         handleInputChange={handleInputChange}
-        placeholder={
-          !multiple && selectedItems?.length
-            ? findOptionLabel(selectedItems[0])
-            : placeholder
-        }
+        placeholder={getPlaceholder()}
       />
       {open && (
         <div className={style.options}>
+          {filteredOptions.map((option) => {
+            const checked = !!selectedItems?.includes(option.id);
+            return (
+              <div
+                className={`${style.option} ${checked ? style.checked : ''}`}
+                key={option.id}
+                onClick={(e) => handleSelect(option, e)}
+              >
+                <Checkbox checked={checked} />
+                {option.label}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TestMultiSelect;
